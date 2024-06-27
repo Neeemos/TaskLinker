f3361314d14b84ddf0a161eff81ee1181900a75a
Neeemos <enzo.belfi@galaxy-hotels.fr>
2024-06-27 17:18:55 +0200

    Form task, controller trask, entity project a modifier

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,37 +2,92 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\ProjectType;
+use App\Form\TaskType;
+use App\Repository\ProjectRepository;
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TaskController extends AbstractController
 {
 
     #[Route('/task/{id}/{statut}/add', name: 'project_task_add')]
-    public function taskFormAdd(request $request, int $id, int $statut): Response
+    public function taskFormAdd(request $request, int $id, int $statut, ProjectRepository $projectRepository, TaskRepository $taskRepository, EntityManagerInterface $entityManager): Response
     {
- 
+        $project = $projectRepository->find($id);
+
+
+        if (!$project) {
+            throw $this->createNotFoundException('No project found');
+        }
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task, ['project' => $project, 'status' => $statut]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setProject($project);
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('project_id', ['id' => $id]);
+        }
+
         return $this->render('task/form.html.twig', [
             'current_route' => $request->attributes->get('_route'),
             'id' => $id,
-            'statut' => $statut
+            'statut' => $statut,
+            'form' => $form->createView()
         ]);
     }
     #[Route('/task/{id}/{taskId}/edit', name: 'project_task_edit')]
-    public function taskFormEdit(request $request, int $id, int $taskId): Response
+    public function taskFormEdit(request $request, int $id, int $taskId, ProjectRepository $projectRepository, TaskRepository $taskRepository, EntityManagerInterface $entityManager): Response
     {
+        $project = $projectRepository->find($id);
+
+
+        if (!$project) {
+            throw $this->createNotFoundException('No project found');
+        }
+
+        $task = $taskRepository->find($taskId);
+        if (!$task) {
+            throw $this->createNotFoundException('No task found');
+        }
+
+        $form = $this->createForm(TaskType::class, $task, ['project' => $project, 'status' => $task->getStatus()]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('project_id', ['id' => $id]);
+        }
+
         return $this->render('task/form.html.twig', [
             'current_route' => $request->attributes->get('_route'),
             'id' => $id,
-            'statut' => $taskId
+            'taskId' => $taskId,
+            'statut' => $task->getStatus(),
+            'form' => $form->createView()
         ]);
     }
-    #[Route('/task/{id}/{statut}/remove', name: 'project_task_remove')]
-    public function taskRemove(int $id, int $statut): Response
+    #[Route('/task/{id}/{taskId}/remove', name: 'project_task_remove')]
+    public function taskRemove(int $id, int $taskId, TaskRepository $repository, EntityManagerInterface $entityManagerInterface): Response
     {
-        /// ajout de la suppression
-        return $this->redirectToRoute('project_id', ['id' => $id, 'statut' => $statut]);
+        $task = $repository->find($taskId);
+
+        if (!$task || $task->getProject()->getId() != $id) {
+            throw $this->createNotFoundException('Task not found or does not belong to the specified project');
+        }
+
+        $entityManagerInterface->remove($task);
+        $entityManagerInterface->flush();
+
+        return $this->redirectToRoute('project_id', ['id' => $id]);
     }
 }
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -7,6 +7,10 @@ use App\Entity\Task;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,20 +18,38 @@ class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $project = $options['project'];
+        $status = $options['status'];
         $builder
-            ->add('title')
+            ->add('title', TextType::class, ['label' => 'Titre de la tâche'])
             ->add('description')
             ->add('date', null, [
                 'widget' => 'single_text',
             ])
-            ->add('status')
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'To do' => '1',
+                    'Doing' => '2',
+                    'Done' => '3',
+                ],
+                'data' => $status,
+            ])
             ->add('user', EntityType::class, [
                 'class' => User::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'label' => 'Membre'
             ])
+            // ->add('project', HiddenType::class, [
+            //     'data' => $project ? $project->getId() : null, // Set the project ID as hidden field value
+            // ])
             ->add('project', EntityType::class, [
                 'class' => Project::class,
                 'choice_label' => 'id',
+            ])
+
+            ->add('submit', SubmitType::class, [
+                'label' => 'Ajouter',
+                'attr' => ['class' => 'button button-submit']
             ])
         ;
     }
@@ -36,6 +58,9 @@ class TaskType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Task::class,
+            'status' => '1',
         ]);
+        $resolver->setRequired('project');
+        $resolver->setAllowedTypes('project', [Project::class, 'null']);
     }
 }
